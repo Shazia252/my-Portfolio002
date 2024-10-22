8a43f5b80b7b842cf9ce8ad05fdaf8c516aee53c
Shazia252 <161409243+Shazia252@users.noreply.github.com>
2024-10-22 11:36:09 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       <div className='text-xl font-medium'>Sultana</div>
       <ul className='gap-10 lg:gap-16 hidden md:flex'>
         <li className='menuLink'><a href='#hero'>Home</a></li>
-        <li className='menuLink'><a href='#about'>About</a></li>
+        <li className='menuLink'><a href='#about'>About Me</a></li>
         <li className='menuLink'><a href='#projects'>Projects</a></li>
         <li className='menuLink'><a href='#skills'>Skills</a></li>
         <li className='menuLink'><a href='#contact'>Contact</a></li>
